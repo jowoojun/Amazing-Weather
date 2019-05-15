2ddb33c9d464155e48bd6045962a931fdeb61e0e
Jo WooJun <jowoojun@naver.com>
2019-05-15 20:06:35 +0900

    completed loading screen

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: false,
+    error: null,
+  };
+
   render() {
+    const {isLoaded, error} = this.state
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {isLoaded ? 
+          null : (
+          <View style={styles.loading}> 
+            <ActivityIndicator style={styles.spinner} size="large" color="#0000ff" />
+            <View style={styles.loadingTextBox}>
+              <Text style={styles.loadingText}>Getting the Amazing Weather!!</Text>
+              {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            </View>
+          </View>
+        )}
       </View>
     );
   }
@@ -14,8 +29,28 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  loading:{
+    flex:1,
+    backgroundColor: '#FDF6AA',
+  },
+  spinner:{
+    flex:1,
+    justifyContent: "center",
+    alignItems:'center',
+  },
+  loadingTextBox:{
+    paddingLeft: 28,
+    paddingBottom: 140,
+  },
+  loadingText:{
+    justifyContent: 'flex-end',
+    marginBottom: 10,
+    fontSize:38
+  },
+  errorText:{
+    color:"red",
+    backgroundColor: "transparent",
+    fontSize:24
   },
 });
